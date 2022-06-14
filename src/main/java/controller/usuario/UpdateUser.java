d9cb527d96255955cd0e9975cf8fa317aac26031
Huan <Huan@10.100.0.26>
2022-06-14 11:47:12 -0300

    Edição de Usuario

---
+++ src/main/java/controller/usuario/UpdateUser.java
@@ -0,0 +1,46 @@
+package controller.usuario;
+
+import DAO.Class.ErroDAO;
+import DAO.Class.UsuarioDAOClass;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Usuario;
+
+@WebServlet(name = "UpdateUser", urlPatterns = {"/updateuser"})
+public class UpdateUser extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            UsuarioDAOClass daoU = new UsuarioDAOClass();
+
+            String nome = (String) request.getAttribute("nome");
+            String endereco = (String) request.getAttribute("endereco");
+            String telefone = (String) request.getAttribute("telefone");
+            String senha = (String) request.getAttribute("senha");
+            String cpf = (String) request.getAttribute("cpf");
+            boolean cliente = (boolean) request.getAttribute("cliente");
+
+            if (nome != null && telefone != null && senha != null && cpf != null) {
+                Usuario u = new Usuario(nome, endereco, telefone, senha, cpf, cliente);
+                daoU.update(u);
+            }
+
+            daoU.sair();
+
+        } catch (ErroDAO ex) {
+            System.out.println(ex);
+            //mensagem de erro para a View
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
