1a9f97eb0432367f3383a0be0f9fd8841c971857
Huan Carlos <huanc65@gmail.com>
2022-06-15 19:49:28 -0300

    marge branch local

--- src/main/java/controller/usuario/Logar.java
+++ src/main/java/controller/usuario/Logar.java
@@ -8,31 +8,17 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-<<<<<<< HEAD
 import jakarta.servlet.http.HttpSession;
 import model.Usuario;
-=======
->>>>>>> 9d86c42de37b3f891783f245126c89687a92b1bc
 
 @WebServlet(name = "Logar", urlPatterns = {"/logar"})
 public class Logar extends HttpServlet {
 
     @Override
-<<<<<<< HEAD
-=======
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if ("1".equals(request.getParameter("flag"))) {
-            response.sendRedirect("login.jsp");
-        }
-    }
-
-    @Override
->>>>>>> 9d86c42de37b3f891783f245126c89687a92b1bc
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
             UsuarioDAOClass daoU = new UsuarioDAOClass();
 
-<<<<<<< HEAD
             String cpf = request.getParameter("cpf");
             String senha = request.getParameter("senha");
             
@@ -43,23 +29,12 @@ public class Logar extends HttpServlet {
                 HttpSession sessao = request.getSession(true);
                 sessao.setAttribute("usuario", use);
                 if (use.isCliente()) {
-=======
-            String cpf = (String) request.getAttribute("cpf");
-            String senha = (String) request.getAttribute("senha");
-
-            if (cpf != null && senha != null) {
-                if (daoU.logar(cpf, senha).isCliente()) {
->>>>>>> 9d86c42de37b3f891783f245126c89687a92b1bc
                     request.getRequestDispatcher("/WEB-INF/view/areacliente.jsp").forward(request, response);
                 } else {
                     request.getRequestDispatcher("/WEB-INF/view/areaatendente.jsp").forward(request, response);
                 }
 
-<<<<<<< HEAD
             } else {
-=======
-            }else{
->>>>>>> 9d86c42de37b3f891783f245126c89687a92b1bc
                 response.sendRedirect("login.jsp?mensagem=Dados Invalidos!");
             }
 
--- src/main/webapp/WEB-INF/view/usuario/createuser.jsp
+++ src/main/webapp/WEB-INF/view/usuario/createuser.jsp
@@ -10,7 +10,6 @@
         <form action="createuser" method="POST">
             <p>Preencha os dados:</p>
             <p>CPF</p>
-<<<<<<< HEAD
             <input type="text" name="cpf" placeholder="Somente Números" required>
             <p>Nome</p>
             <input type="text"  name="nome" placeholder="Informe o Nome" required>
@@ -20,26 +19,12 @@
             <input type="text" name="telefone" placeholder="Somente Números" required>
             <p>Senha</p>
             <input type="password" name="senha" placeholder="Informe a Senha" required>
-=======
-            <input type="text" placeholder="Somente Números" required>
-            <p>Nome</p>
-            <input type="text" placeholder="Informe o Nome" required>
-            <p>Endereço</p>
-            <input type="text" placeholder="Informe o Endereço">
-            <p>Telefone</p>
-            <input type="text" placeholder="Somente Números" required>
-            <p>Senha</p>
-            <input type="password" placeholder="Informe a Senha" required>
->>>>>>> 9d86c42de37b3f891783f245126c89687a92b1bc
             <p>Tipo de Usuário</p>
             <input type="radio" name="cliente" value="true" required>
             <label>Cliente</label>
             <input type="radio" name="cliente" value="false" required>
             <label>Atendente</label>
-<<<<<<< HEAD
             <input type="submit" value="Enviar">
-=======
->>>>>>> 9d86c42de37b3f891783f245126c89687a92b1bc
         </form>
     </body>
 </html>
