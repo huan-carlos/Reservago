bc0dfb92ee56593c51b95f3a1c8109178b2eb711
Huan Carlos <huanc65@gmail.com>
2022-06-19 15:54:13 -0300

    Ajuste na verificação do usuário que deleta usuários

--- src/main/java/controller/usuario/DeleteUser.java
+++ src/main/java/controller/usuario/DeleteUser.java
@@ -13,7 +13,7 @@ import model.Usuario;
 
 @WebServlet(name = "DeleteUser", urlPatterns = {"/deleteuser"})
 public class DeleteUser extends HttpServlet {
-    
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request, response);
@@ -22,29 +22,25 @@ public class DeleteUser extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession sessao = request.getSession(false);
+        Usuario use = (Usuario) sessao.getAttribute("usuario");
 
-        if (sessao != null) {
-            Usuario u = (Usuario) sessao.getAttribute("usuario");
-            if (!u.isCliente()) {
-                try {
-                    UsuarioDAOClass daoU = new UsuarioDAOClass();
+        if (use != null && !use.isCliente()) {
+            try {
+                UsuarioDAOClass daoU = new UsuarioDAOClass();
+                String cpf = request.getParameter("cpf");
 
-                    String cpf = request.getParameter("cpf");
-
-                    if (cpf != null) {
-                        daoU.delete(cpf);
-                    }
-
-                    daoU.sair();
-                    
-                    response.sendRedirect("readuser");
-
-                } catch (ErroDAO ex) {
-                    System.out.println(ex);
-                    //mensagem de erro para a View
+                if (cpf != null) {
+                    daoU.delete(cpf);
                 }
+                daoU.sair();
+                response.sendRedirect("readuser");
+
+            } catch (ErroDAO ex) {
+                System.out.println(ex);
+                //mensagem de erro para a View
             }
         }
+
     }
 
     @Override
